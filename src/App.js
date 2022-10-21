1243a65b0eb7ef41b380ef6d5e0ad02e65c2c923
edzonee <edzone1996@gmail.com>
2022-10-21 13:26:15 +0200

    Added modal instead of alerts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DepositForm from "./Components/DepositForm";
 import WithdrawForm from "./Components/WithdrawForm";
+import Modal from "./Components/Modal";
 import "./Styles/App.css";
 
 class App extends React.Component {
@@ -11,6 +12,10 @@ class App extends React.Component {
       addBalance: 0,
       addExpense: 0,
       expensesList: [],
+      showModalMinus: false,
+      showModalInsufficient: false,
+      showModalDeclined: false,
+      showModalExist: false,
     };
   }
 
@@ -23,20 +28,36 @@ class App extends React.Component {
 
   addExpenses = (name, amount) => {
     if (this.state.addBalance < amount) {
-      alert("Declined");
+      this.setState({ showModalDeclined: true });
+      let audio = new Audio("./sus.mp3");
+      audio.play();
       return;
     }
-    if (amount < 0){
-      alert('🤨🤨🤨🤨🤨')
+    if (amount < 0) {
+      this.setState({ showModalMinus: true });
+      let audio = new Audio("./vine-boom.mp3");
+      let audio2 = new Audio("./movie_1.mp3");
+      audio.play();
+      audio2.play();
       return;
     }
     if (amount === 0) {
-      alert(`Hey you can't buy stuff for free!`);
+      this.setState({ showModalInsufficient: true });
+      let audio = new Audio("./vine-boom.mp3");
+      let audio2 = new Audio("./movie_1.mp3");
+      let audio3 = new Audio("./aughhhh-tiktok.mp3");
+      audio.play();
+      audio2.play();
+      audio3.play();
       return;
     }
     for (const item of this.state.expensesList) {
-      if (item.name === name){
-        alert('Item exisististi')
+      if (item.name === name) {
+        this.setState({ showModalExist: true });
+        let audio = new Audio("./sus.mp3");
+        let audio2 = new Audio("./vine-boom.mp3");
+        audio.play();
+        audio2.play();
         return;
       }
     }
@@ -52,12 +73,22 @@ class App extends React.Component {
     });
   };
 
+  hideModal = () => {
+    this.setState({ showModalMinus: false });
+    this.setState({ showModalInsufficient: false });
+    this.setState({ showModalDeclined: false });
+    this.setState({ showModalExist: false });
+  };
+
   renderMyItems = () => {
     return this.state.expensesList.map((value, index) => {
       return (
-        <div key={index}>
-          <p>Item name: {value.name}</p>
-          <p>Amount: {value.amount}</p>
+        <div className="my-list" key={index}>
+          <ul>
+            <p>Item name: {value.name}</p>
+            <p>Amount: {value.amount}</p>
+          </ul>
+
           {console.log(value.name)}
         </div>
       );
@@ -73,7 +104,7 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className="container">
         {" "}
         <DepositForm addBudgetAndBalance={this.addBudgetAndBalance} />
         <WithdrawForm addExpenses={this.addExpenses} />
@@ -83,6 +114,37 @@ class App extends React.Component {
           <h1>Balance: {this.state.addBalance} €</h1>
         </div>
         {this.renderMyItems()}
+        {this.state.showModalMinus && (
+          <Modal
+            btnText="I'm sorry Mr. Rock"
+            imgUrl="https://media.tenor.com/QA_IqSKoWTcAAAAC/the-rock.gif"
+            hideModal={this.hideModal}
+          />
+        )}
+        {this.state.showModalInsufficient && (
+          <Modal
+            titleTxt="🤨🤨🤨🤨"
+            btnText="I'm sorry Mr Rock, I will get some money"
+            imgUrl="https://media.tenor.com/ssA9ZQag3Z0AAAAd/dwayne-johnson.gif"
+            hideModal={this.hideModal}
+          />
+        )}
+        {this.state.showModalDeclined && (
+          <Modal
+            titleTxt="Nice try"
+            btnText="Okay..."
+            imgUrl="https://www.kibrispdr.org/data/1791/nodding-gif-1.gif"
+            hideModal={this.hideModal}
+          />
+        )}
+        {this.state.showModalExist && (
+          <Modal
+            titleTxt="Item already exists??"
+            btnText="Sorry Mr. Rock, I will add something else..."
+            imgUrl="https://media.tenor.com/XGpqtoboIiUAAAAC/the-rock-dwayne-johnson.gif"
+            hideModal={this.hideModal}
+          />
+        )}
       </div>
     );
   }
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../Styles/Modal.css";
+
+class Modal extends React.Component {
+  render() {
+    return (
+      <div className="modal">
+        <h1>{this.props.titleTxt}</h1>
+        <img src={this.props.imgUrl} alt="The Rock" />
+        <button className="modal-btn" onClick={this.props.hideModal}>
+          {this.props.btnText}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Modal;
